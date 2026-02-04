4d5dc4c48b82f40017a5ea1b80e93aeeb6785049
Jesuit Student 26317 <26317@jcpstudents.org>
2026-02-03 20:09:26 -0600

    Add all the test motors

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,13 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
-  private final TalonFXMotor motor = new TalonFXMotor(0);
+  private final TalonFXMotor intake = new TalonFXMotor(43);
+  private final TalonFXMotor mBottom = new TalonFXMotor(40); 
+  private final TalonFXMotor mTop = new TalonFXMotor(42); 
+  private final TalonFXMotor hTop = new TalonFXMotor(41);
+  private final TalonFXMotor hBottom = new TalonFXMotor(44);
+  private final TalonFXMotor shooter1 = new TalonFXMotor(50);
+  private final TalonFXMotor shooter2 = new TalonFXMotor(51);
 
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
@@ -18,7 +24,16 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
-    motor.setDefaultCommand(motor.setOutput(() -> m_driverController.getLeftY() * -1));
+
+    intake.setDefaultCommand(intake.setOutput(() -> m_driverController.getLeftY() * -1 * -0.66));
+    
+    mBottom.setDefaultCommand(mBottom.setOutput(() -> m_driverController.getRightY() * -1 * -0.66));
+    mTop.setDefaultCommand(mTop.setOutput(() -> m_driverController.getRightY() * -1 * -0.66));
+    hTop.setDefaultCommand(hTop.setOutput(() -> m_driverController.getRightY() * -1 * 0.66));
+    hBottom.setDefaultCommand(hBottom.setOutput(() -> m_driverController.getRightY() * -1 * 0.66));
+
+    shooter1.setDefaultCommand(shooter1.setOutput(() -> m_driverController.getRightTriggerAxis() * -1));
+    shooter2.setDefaultCommand(shooter2.setOutput(() -> m_driverController.getRightTriggerAxis() * 1));
   }
 
   public Command getAutonomousCommand() {
--- src/main/java/frc/robot/subsystems/TalonFXMotor.java
+++ src/main/java/frc/robot/subsystems/TalonFXMotor.java
@@ -6,6 +6,7 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -14,8 +15,10 @@ public class TalonFXMotor extends SubsystemBase {
     private final TalonFX talonFx;
     private final TalonFXConfiguration talonFxConfig = new TalonFXConfiguration();
     private final DutyCycleOut talonFxOut = new DutyCycleOut(0.0);   
+    private final int myId;
 
     public TalonFXMotor(int id) {
+        myId = id;
         talonFx = new TalonFX(id);
         talonFx.getConfigurator().apply(talonFxConfig);
     }
@@ -24,8 +27,18 @@ public class TalonFXMotor extends SubsystemBase {
         return Commands.run(() -> {
             talonFxOut.Output = output.getAsDouble();   
             talonFx.setControl(talonFxOut);   
-        }
+        },
+        this
             );
+    }
+
+    public double getRPM(){
+        return talonFx.getVelocity().getValueAsDouble()*60;
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber(myId + " rpm", getRPM());
     }
 
 }
