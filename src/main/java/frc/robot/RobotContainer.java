6d9288ddf3f2f4602505e833c0f145187cc5ed6e
Jesuit Student 26317 <26317@jcpstudents.org>
2026-02-05 19:57:47 -0600

    Renamevariables

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,10 +9,10 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
   private final TalonFXMotor intake = new TalonFXMotor(43);
-  private final TalonFXMotor mBottom = new TalonFXMotor(40); 
-  private final TalonFXMotor mTop = new TalonFXMotor(42); 
-  private final TalonFXMotor hTop = new TalonFXMotor(41);
-  private final TalonFXMotor hBottom = new TalonFXMotor(44);
+  private final TalonFXMotor magazineBottom = new TalonFXMotor(40); 
+  private final TalonFXMotor magazineTop = new TalonFXMotor(42); 
+  private final TalonFXMotor hopperTop = new TalonFXMotor(41);
+  private final TalonFXMotor hopperBottom = new TalonFXMotor(44);
   private final TalonFXMotor shooter1 = new TalonFXMotor(50);
   private final TalonFXMotor shooter2 = new TalonFXMotor(51);
 
@@ -27,10 +27,10 @@ public class RobotContainer {
 
     intake.setDefaultCommand(intake.setOutput(() -> m_driverController.getLeftY() * -1 * -0.66));
     
-    mBottom.setDefaultCommand(mBottom.setOutput(() -> m_driverController.getRightY() * -1 * -0.66));
-    mTop.setDefaultCommand(mTop.setOutput(() -> m_driverController.getRightY() * -1 * -0.66));
-    hTop.setDefaultCommand(hTop.setOutput(() -> m_driverController.getRightY() * -1 * 0.66));
-    hBottom.setDefaultCommand(hBottom.setOutput(() -> m_driverController.getRightY() * -1 * 0.66));
+    magazineBottom.setDefaultCommand(magazineBottom.setOutput(() -> m_driverController.getRightY() * -1 * -0.66));
+    magazineTop.setDefaultCommand(magazineTop.setOutput(() -> m_driverController.getRightY() * -1 * -0.66));
+    hopperTop.setDefaultCommand(hopperTop.setOutput(() -> m_driverController.getRightY() * -1 * 0.66));
+    hopperBottom.setDefaultCommand(hopperBottom.setOutput(() -> m_driverController.getRightY() * -1 * 0.66));
 
     shooter1.setDefaultCommand(shooter1.setOutput(() -> m_driverController.getRightTriggerAxis() * -1));
     shooter2.setDefaultCommand(shooter2.setOutput(() -> m_driverController.getRightTriggerAxis() * 1));
