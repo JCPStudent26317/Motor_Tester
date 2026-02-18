ca0f5c68c5e77a8d7ad7812ee2132596463a8d54
Jesuit Student 26317 <26317@jcpstudents.org>
2026-02-18 17:37:06 -0600

    Changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,15 +9,18 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
-  private final TalonFXMotor intake = new TalonFXMotor(43);
-  private final TalonFXMotor magazineBottom = new TalonFXMotor(40); 
-  private final TalonFXMotor magazineTop = new TalonFXMotor(42); 
-  private final TalonFXMotor hopperTop = new TalonFXMotor(41);
-  private final TalonFXMotor hopperBottom = new TalonFXMotor(44);
+  // private final TalonFXMotor intake = new TalonFXMotor(43);
+  // private final TalonFXMotor magazineBottom = new TalonFXMotor(40); 
+  // private final TalonFXMotor magazineTop = new TalonFXMotor(42); 
+  // private final TalonFXMotor hopperTop = new TalonFXMotor(41);
+  // private final TalonFXMotor hopperBottom = new TalonFXMotor(44);
+
+  private final TalonFXMotor shooterTransition = new TalonFXMotor(0);
+
   private final TalonFXMotor shooter1 = new TalonFXMotor(50);
   private final TalonFXMotor shooter2 = new TalonFXMotor(51);
 
-  private final TalonFXMotor climber = new TalonFXMotor(3);
+  // private final TalonFXMotor climber = new TalonFXMotor(3);
 
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
@@ -35,10 +38,12 @@ public class RobotContainer {
     // hopperTop.setDefaultCommand(hopperTop.setOutput(() -> m_driverController.getRightY() * -1 * 0.66));
     // hopperBottom.setDefaultCommand(hopperBottom.setOutput(() -> m_driverController.getRightY() * -1 * 0.66));
 
-    // shooter1.setDefaultCommand(shooter1.setOutput(() -> m_driverController.getRightTriggerAxis() * -1));
-    // shooter2.setDefaultCommand(shooter2.setOutput(() -> m_driverController.getRightTriggerAxis() * 1));
+    shooterTransition.setDefaultCommand(shooterTransition.setOutput(() -> m_driverController.getRightY() * -1));
 
-    climber.setDefaultCommand(climber.setOutput(() -> m_driverController.getLeftY() * -1 * -1));
+    shooter1.setDefaultCommand(shooter1.setOutput(() -> m_driverController.getRightTriggerAxis() * -1));
+    shooter2.setDefaultCommand(shooter2.setOutput(() -> m_driverController.getRightTriggerAxis() * 1));
+
+    // climber.setDefaultCommand(climber.setOutput(() -> m_driverController.getLeftY() * -1 * -1));
   }
 
   public Command getAutonomousCommand() {
